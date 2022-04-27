1d47dfb2643657f1901f6c6eadf983d112bcccd3
Brien Thomas <thomasbrien94@gmail.com>
2022-04-27 15:48:44 -0700

    middleDropDown event listener created: changes the background image and increments the count

--- app.js
+++ app.js
@@ -11,28 +11,33 @@ const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
 // set state for how many times the user changes the head, middle, and bottom
+let headCount = 0;
+let middleCount = 0;
+let bottomCount = 0;
 // set state for all of the character's catchphrases
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
     console.log('changing the selection', headDropdown.value);
-    headEl.style.backgroundImage = `url('./assets/${headDropdown.value}.png')`;
     // increment the head change count state
-    
+    headCount++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
+    headEl.style.backgroundImage = `url('./assets/${headDropdown.value}.png')`;
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
 middleDropdown.addEventListener('change', () => {
     // get the value of the middle dropdown
-
+    console.log('changing the middle selection', middleDropdown.value)
     // increment the middle change count state
-    
+    middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url('./assets/${middleDropdown.value}.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
@@ -59,6 +64,7 @@ catchphraseButton.addEventListener('click', () => {
 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
+    reportEl.textContent = `You have changed the head ${headCount} times, the middle ${middleCount} times, and the bottom ${bottomCount} times.`
 }
 
 function displayCatchphrases() {
